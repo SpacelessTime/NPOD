b48cb66a58c93df20150b6b7ae1cb708fd687d5d
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2022-05-30 17:45:13 -0400

    bulk work

---
+++ src/Body.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Body = ({ title, explanation, copyright, url,quote}) => {
+
+   
+    return(
+    <div className="body-container">
+        <h1 className = 'bodyTitle'> {title} </h1>
+        <img src={url} alt={title} />
+        <h3> By: {copyright} </h3>
+        <p className="bodyText"> {explanation} </p>
+    </div>
+)
+}
+
+export default Body;
