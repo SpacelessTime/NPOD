d1bd077e2693a6a26874e431ac7d83c22620ce6e
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2022-06-06 18:32:55 -0400

    space

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = ({ date, title  }) => {
     return (
     <div className="footer">
-        <h5 className="copyright">{title},{date}</h5>
+        <h5 className="copyright">{title}, {date}</h5>
     </div>
  )}
 export default Footer;
