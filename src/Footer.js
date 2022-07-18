b90db797f38e95d872ee004ec2c90136f6b2e557
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2022-07-18 12:33:31 -0400

    about me page done

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,13 @@
 import React from "react";
+
 const Footer = ({ date, title  }) => {
     return (
-    <div className="footer">
-        <h5 className="copyright">{title}, {date}</h5>
-    </div>
+    <div className="body"><div className="footer">
+            <h5 className="copyright">{title}, {date}</h5> 
+             <a className="photoLink" href='https://profwebsite.vercel.app/'>Checkout my photography here!</a>
+</div>
+                                 
+</div>
+
  )}
 export default Footer;
